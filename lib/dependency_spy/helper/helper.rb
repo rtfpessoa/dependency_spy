06da0c2a827032d2e29bd1470bb98d9dcc07384b
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2018-10-27 18:58:42 -0500

    Allow to configure severity threshold
    * Fixing Rubocop Failures.

--- lib/dependency_spy/helper/helper.rb
+++ lib/dependency_spy/helper/helper.rb
@@ -5,6 +5,7 @@ module DependencySpy
       return true if severity_threshold == 'low' || severity == 'unknown'
       return ['medium', 'high'].include? severity if severity_threshold == 'medium'
       return severity == 'high' if severity_threshold == 'high'
+
       false
     end
 
