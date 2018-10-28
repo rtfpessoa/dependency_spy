4c6acd333aafdabde2b1ae712b7383adc2a051b6
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2018-10-28 16:34:46 +0000

    Update tests with last db fixes

--- spec/dependency_spy_spec.rb
+++ spec/dependency_spy_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe DependencySpy::API do
       manifests       = detected_manifests.select { |m| m.platform == 'npm' }
       dependencies    = manifests.map(&:dependencies).flatten
       vulnerabilities = dependencies.map(&:vulnerabilities).flatten
-      expect(vulnerabilities).to have(87).items
+      expect(vulnerabilities).to have(62).items
     end
 
     it 'can read all dependencies for rubygems manifest' do
